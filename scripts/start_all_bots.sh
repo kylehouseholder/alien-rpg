c4fb524942d18b9d25cf61e14d2084bb8d1fda5f
zr <zt.robinson@outlook.com>
2025-05-23 20:49:32 -0500

    Add scripts to auto-update bot-logs Discord server channel with bot statuses

---
+++ scripts/start_all_bots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "[INFO] Starting all bots..."
+~/alien-rpg/scripts/botctl.sh start
+~/alien-rpg/bots/gem_9k/scripts/botctl.sh start
+~/alien-rpg/bots/murray_2.0/scripts/botctl.sh start
---
+++ scripts/stop_all_bots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "[INFO] Stopping all bots..."
+~/alien-rpg/scripts/botctl.sh stop
+~/alien-rpg/bots/gem_9k/scripts/botctl.sh stop
+~/alien-rpg/bots/murray_2.0/scripts/botctl.sh stop
